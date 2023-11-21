8080b449117502263cd4c6f2ca328c764c6d4204
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-21 14:48:26 -0600

    add 2-export_to_JSON file

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+Script to export user-specific data from the JSONPlaceholder 
+API in JSON format.
+
+Functions:
+    export_data_to_json(user_id):
+        Exports user-related data, including completed 
+        tasks, to a JSON file.
+        Args:
+            user_id (int): The ID of the user for whom 
+            data is exported.
+
+Usage:
+    python script_name.py user_id
+"""
+import json
+import requests
+from sys import argv
+
+BASE_API_URL = 'https://jsonplaceholder.typicode.com'
+
+
+def export_data_to_json(user_id):
+    """
+    Export user-related data to a JSON file.
+
+    Args:
+        user_id (int): The ID of the user for whom data is exported.
+
+    Raises:
+        requests.exceptions.RequestException: If there is 
+        an issue with API requests.
+
+    Usage:
+        export_data_to_json(1)
+    """
+    try:
+        user_response = requests.get(f"{BASE_API_URL}/users/{user_id}")
+        user_response.raise_for_status()
+        user_data = user_response.json()
+
+        todo_response = requests.get(f"{BASE_API_URL}/todos?userId={user_id}")
+        todo_response.raise_for_status()
+        todo_data = todo_response.json()
+
+        data = {
+            user_id: [
+                {
+                    "task": task['title'],
+                    "completed": task['completed'],
+                    "username": user_data['username']
+                }
+                for task in todo_data
+            ]
+        }
+
+        with open(f"{user_id}_exported_data.json", mode='w') as json_file:
+            json.dump(data, json_file)
+
+        print(f"Data has been exported to {user_id}_exported_data.json")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: Unable to fetch data from the API. {e}")
+
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print("Usage: python script_name.py user_id")
+        exit(1)
+
+    try:
+        user_id = int(argv[1])
+        export_data_to_json(user_id)
+    except ValueError:
+        print("Error: User ID must be an integer.")
+        exit(1)
