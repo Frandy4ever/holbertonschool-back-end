f7cbba4c07afecb360b6550a8167a7c822cd9cb3
Frandy Slueue <frandy4ever@gmail.com>
2023-11-21 02:18:54 -0600

    update 0-gather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,29 +10,22 @@ def display_employee_progress(employee_id):
     todo_url = f"{url}/todos"
     employee_url = f"{url}/users/{employee_id}"
 
-    try:
-        employee_data = requests.get(employee_url).json()
-        todo_data = requests.get(
-            todo_url, params={"userId": employee_id}).json()
+    todo_data = requests.get(
+        todo_url, params={"userId": employee_id}).json()
+    employee_data = requests.get(employee_url).json()
 
-        employee_name = employee_data.get("name")
-        completed_tasks = [i["title"] for i in todo_data if i["completed"]]
-        completed = len(completed_tasks)
-        total_complete = len(todo_data)
+    employee_name = employee_data.get("name")
+    completed_tasks = [i["title"] for i in todo_data if i["completed"]]
+    completed = len(completed_tasks)
+    total_complete = len(todo_data)
 
-        print(f"Employee {employee_name} is done with tasks({completed}/{total_complete})")
-        for task in completed_tasks:
-            print(f"\t {task}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        sys.exit(1)
+    print(
+        f"Employee {employee_name} is done with tasks({completed}/{total_complete})")
+    for task in completed_tasks:
+        print(f"\t {task}")
 
 
 if __name__ == "__main__":
     import sys
-    # if len(sys.argv) != 2:
-    #     print("Usage: python script.py <employee_id>")
-    #     sys.exit(1)
 
     display_employee_progress(int(sys.argv[1]))
